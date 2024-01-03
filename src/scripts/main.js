bc2a59dfc91cc5bc41d80dedee89432e0b00c48f
Vitor Brito <joaovitor16611661@gmail.com>
2024-01-02 21:36:55 -0300

    finalização do projeto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,90 @@
 $(document).ready(function(){
+
+    $(".header__nav__mobile").click(function () {
+        $('.header__nav__itens').slideToggle();
+    });
+    
     $('.carousel__images').slick({
         autoplay:true,
         prevArrow: 'null',
         nextArrow: 'null'
     });
+
+
+    $('.characters__cards').slick({
+        centerMode: true,
+        centerPadding: 'null',
+        slidesToShow: 3,
+            responsive: [
+                {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                },
+                },
+            ]
+    })
+
+    responsiveResolu();
+
+    $(window).resize(function () {
+        responsiveResolu();
+    });
+
+    function responsiveResolu () {
+        var screenWidth = $(window).width();
+        
+        if (screenWidth <= 1023) {
+
+            $('.characters__cards').off('beforeChange');
+            $('.characters__cards').off('afterChange');
+            $('.slick-center').css('transform', 'scale(1)');
+            
+            $('.header__nav__itens').hide();
+
+        } else {
+
+            $('.characters__cards').on('init beforeChange', function(event, slick, currentSlide, nextSlide){
+                $('.slick-center').css('transform', 'scale(1)');
+            });
+            
+            $('.characters__cards').on('afterChange', function(event, slick, currentSlide){
+                $('.slick-center').css('transform', 'scale(1.2)');
+            });
+
+            $('.slick-center').css('transform', 'scale(1.2)');
+
+            $('.header__nav__itens').show();
+        }
+    }
+    
+
+
+
+
+    $('nav a').on('click', function(e) {
+        e.preventDefault();
+
+        var targetSectionId = $(this).attr('href').substring(1);
+        var targetSection = $('#' + targetSectionId);
+
+        var headerHeight = $('header').outerHeight();
+
+        var windowHeight = $(window).height();
+
+        var sectionHeight = targetSection.outerHeight();
+
+        var scrollToPosition = targetSection.offset().top - headerHeight;
+
+        if (sectionHeight < windowHeight) {
+            scrollToPosition -= (windowHeight - sectionHeight) / 2;
+        }
+
+        $('html, body').animate({
+            scrollTop: scrollToPosition
+        }, 500);
+    });
+
 })
